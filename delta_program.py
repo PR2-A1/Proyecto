90fa555319aaacacb44e78f04c9b536e4fcd706c
DiegoJimenezRomero <djimrom@etsinf.upv.es>
2026-05-07 15:36:07 +0200

    Código del delta completo

--- delta_program.py
+++ delta_program.py
@@ -6,13 +6,46 @@ from robolink import *
 import math
 import numpy as np
 
+#NAME CONFIGURATIONS
 ROBOT_NAME = 'ABB IRB 360-1/1600 4D'
+TOOL_NAME = 'RobotiQ EPick Vacuum Gripper (1 Cup)'
 CAMERA_NAME = 'camera_frame'
+PICK_FRAME = 'frame_pick_delta'
+PRE_PICK_TARGET_NAME = 'pre_pick_delta'
+PICK_TARGET_NAME = 'pick_delta'
+POST_PICK_TARGET_NAME = 'post_pick_delta'
+ROBOT_PARENT_FRAME_NAME = 'ABB IRB 360-1/1600 4D Base'
 
+#OBJECTS
 robot = RDK.Item(ROBOT_NAME, ITEM_TYPE_ROBOT)
+tool = RDK.Item(TOOL_NAME, ITEM_TYPE_TOOL)
 camera = RDK.Item(CAMERA_NAME, ITEM_TYPE_FRAME)
 
-z_distance_camera_to_cap = 1042.302
+#FRAMES
+robot_frame = RDK.Item(ROBOT_PARENT_FRAME_NAME, ITEM_TYPE_FRAME)
+pick_frame = RDK.Item(PICK_FRAME, ITEM_TYPE_FRAME)
+pre_pick_target = RDK.Item(PRE_PICK_TARGET_NAME, ITEM_TYPE_TARGET)
+pick_target = RDK.Item(PICK_TARGET_NAME, ITEM_TYPE_TARGET)
+post_pick_target = RDK.Item(POST_PICK_TARGET_NAME, ITEM_TYPE_NAME)
+
+#CONFIGURATION
+z_distance_camera_to_cap = 912.302
+
+def pick_function():
+	#CHANGE THE ROBOT'S REFERENCE FRAME TO THE PICK FRAME
+	robot.setPoseFrame(pick_frame)
+	
+	#EXECUTE THE PICK CYCLE
+	robot.MoveJ(pre_pick_target)
+	robot.MoveL(pick_target)
+	tool.AttachClosest("tapon_")
+	robot.MoveL(post_pick_target)
+	
+	#CHANGE BACK TO THE ROBOT'S PARENT REFERENCE FRAME
+	robot.setPoseFrame(robot_frame)
+	
+def place_function():
+	robot.setPoseFrame(place_frame)
 
 while 1:
 	x = RDK.getParam('camera_x')
@@ -28,7 +61,9 @@ while 1:
 	
 	cap_robot_position = robot_position.inv() * cap_absolute_position
 	
-	final_pose = robot.Pose() # Get current orientation
-	final_pose.setPos(cap_robot_position.Pos()) # Swap only the position
+	#CHANGE THE PICK FRAME POSITION
+	pick_frame.setPos(cap_robot_position.Pos())
+	
+	pick_function()
 	
 	robot.MoveJ(final_pose)
